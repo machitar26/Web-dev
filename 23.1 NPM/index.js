b9f8e5de88039111a63edbadc383902e7435d592
Erwin Machitar <erwinmachitar@gmail.com>
2025-07-05 03:55:23 +0800

    Add 23.0 Using Node and 23.1 NPM

---
+++ 23.1 NPM/index.js
@@ -0,0 +1,8 @@
+import generateName from "sillyname";
+import { randomSuperhero } from "superheroes";
+
+const sillyName = generateName();
+
+console.log(`My name is ${sillyName}.`);
+
+console.log(`I am ${randomSuperhero()}!`);
